15c23fab2989ce5aad7d6c8af90107a3d670e721
Ilya Spivakov <xaker00@gmail.com>
2021-11-13 22:42:06 -0800

    updated server

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,8 @@
+const { model } = require('mongoose');
 const User = require('./User');
+const bookSchema = require('./Book');
 
-module.exports = { User };
+const Book = model('Book', bookSchema);
+
+
+module.exports = { User, Book };
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,36 +1,32 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 // set token secret and expiration date
-const secret = 'mysecretsshhhhh';
-const expiration = '2h';
+const secret = process.env.JWT_SECRET || "mysecretsshhhhh";
+const expiration = "2h";
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.query?.token || req.headers?.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+    if (req.headers?.authorization) {
+      token = token.split(" ").pop().trim();
     }
 
     // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+    if (token) {
+      try {
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        req.user = data;
+      } catch {
+        console.log("Invalid token");
+      }
     }
 
-    // send to next endpoint
-    next();
+    return req;
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
